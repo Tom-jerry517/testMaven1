6c4bb725f81c2ae341bf5285cc57b222ac047d61
cy <13120489566@163.com>
2022-08-12 13:10:47 +0800

    Initial commit

---
+++ src/main/java/com/example/Hello.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Hello {
+
+    public void say(){
+        System.out.println("hello,maven!");
+    }
+}
---
+++ src/test/java/com/example/HelloTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.Test;
+
+public class HelloTest {
+
+    @Test
+    public void test(){
+        Hello hello=new Hello();
+        hello.say();
+    }
+}
